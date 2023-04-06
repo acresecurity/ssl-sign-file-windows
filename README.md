febbf8cc75169219bf3c222c080a0cb896c112f2
JaspreetSanghra <JaspreetSanghra@users.noreply.github.com>
2023-04-06 09:12:59 -0400

    Initial implementation - Sign File on Windows Runner Action (#1)
    * Skeleton logic
    
    * .gitignore is now working
    
    * initial test case
    
    * Remove args
    
    * tweak index path
    
    * add dist
    
    * Update conf file path
    
    * log conf file path
    
    * Updat conf path again
    
    * Updates for directory path issues
    
    * update zip location
    
    * undo download location
    
    * log dir and files
    
    * log dist
    
    * use pwd for dir
    
    * verifying error on actions run
    
    * lof firname
    
    * Use dirname
    
    * remove bad logs
    
    * exec with full path
    
    * Check for error string
    
    * Update to handle real use case
    
    * convert istest to bool
    
    * Update dist
    
    * Remove test files
    
    * Clean up index and fix ReadMe
    
    * Adjust istest settings in action.yml

--- README.md
+++ README.md
@@ -1,116 +1,46 @@
-# Create a JavaScript Action
+# ssl-sign-file-windows
 
-<p align="center">
-  <a href="https://github.com/actions/javascript-action/actions"><img alt="javscript-action status" src="https://github.com/actions/javascript-action/workflows/units-test/badge.svg"></a>
-</p>
+Sign a file using SSL.com's CodeSignTool for Windows. Use this to sign a file with an action running on a windows image.
 
-Use this template to bootstrap the creation of a JavaScript action.:rocket:
+| Environment | Action To Use                                       |
+|-------------|-----------------------------------------------------|
+| Linux       | https://github.com/feenicsinc/ssl-sign-file         |
+| Windows     | https://github.com/feenicsinc/ssl-sign-file-windows | 
 
-This template includes tests, linting, a validation workflow, publishing, and versioning guidance.
+## Inputs
 
-If you are new, there's also a simpler introduction.  See the [Hello World JavaScript Action](https://github.com/actions/hello-world-javascript-action)
+### `filepath`
 
-## Create an action from this template
+**Required** File to be signed.
 
-Click the `Use this Template` and provide the new repo details for your action
+### `sslusername`
 
-## Code in Main
+**Required** SSL.com account username.
 
-Install the dependencies
+### `sslpassword`
 
-```bash
-npm install
-```
+**Required** SSL.com account password.
 
-Run the tests :heavy_check_mark:
+### `sslsecretpassword`
 
-```bash
-$ npm test
+**Required** SSL.com account TOTP secret.
 
- PASS  ./index.test.js
-  ✓ throws invalid number (3ms)
-  ✓ wait 500 ms (504ms)
-  ✓ test runs (95ms)
-...
-```
+### `istest`
 
-## Change action.yml
+When `false`, runs against SSL.com Production account.  Default `true`.
 
-The action.yml defines the inputs and output for your action.
+## Outputs
 
-Update the action.yml with your name, description, inputs and outputs for your action.
+## none
 
-See the [documentation](https://help.github.com/en/articles/metadata-syntax-for-github-actions)
+## Example usage
 
-## Change the Code
-
-Most toolkit and CI/CD operations involve async operations so the action is run in an async function.
-
-```javascript
-const core = require('@actions/core');
-...
-
-async function run() {
-  try {
-      ...
-  }
-  catch (error) {
-    core.setFailed(error.message);
-  }
-}
-
-run()
-```
-
-See the [toolkit documentation](https://github.com/actions/toolkit/blob/master/README.md#packages) for the various packages.
-
-## Package for distribution
-
-GitHub Actions will run the entry point from the action.yml. Packaging assembles the code into one file that can be checked in to Git, enabling fast and reliable execution and preventing the need to check in node_modules.
-
-Actions are run from GitHub repos.  Packaging the action will create a packaged action in the dist folder.
-
-Run prepare
-
-```bash
-npm run prepare
-```
-
-Since the packaged index.js is run from the dist folder.
-
-```bash
-git add dist
-```
-
-## Create a release branch
-
-Users shouldn't consume the action from master since that would be latest code and actions can break compatibility between major versions.
-
-Checkin to the v1 release branch
-
-```bash
-git checkout -b v1
-git commit -a -m "v1 release"
-```
-
-```bash
-git push origin v1
-```
-
-Note: We recommend using the `--license` option for ncc, which will create a license file for all of the production node modules used in your project.
-
-Your action is now published! :rocket:
-
-See the [versioning documentation](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md)
-
-## Usage
-
-You can now consume the action by referencing the v1 branch
-
-```yaml
-uses: actions/javascript-action@v1
-with:
-  milliseconds: 1000
-```
-
-See the [actions tab](https://github.com/actions/javascript-action/actions) for runs of this action! :rocket:
+        - id: Sign_Feenics_Keep_Windows_exe
+          uses: feenicsinc/ssl-sign-file-windows@latest
+          with:
+            filepath: "${{ github.workspace }}\\Feenics.Keep.Windows\\Feenics.Keep.Windows\\bin\\x86\\DEV\\Feenics.Keep.Windows.exe"
+            sslusername: ${{ secrets.SSL_USRNM }}
+            sslpassword: ${{ secrets.SSL_PWD }}
+            sslsecretpassword: ${{ secrets.SSL_TKN }}
+            sslclientid: ${{ secrets.SSL_CLIENT_ID }}
+            istest: false
--- index.js
+++ index.js
@@ -1,20 +1,123 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
+const core = require("@actions/core");
+const wait = require("./wait");
+const admZip = require("adm-zip");
+const request = require("superagent");
+const fs = require("fs");
+const path = require("path");
+let exec = require("child_process").exec;
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    core.info(`---START`);
+    const filePath =
+      core.getInput("filepath") ||
+      path.resolve(__dirname, "..") + "\\fake-file.ps1";
+    const sslUsername = core.getInput("sslusername");
+    const sslPassword = core.getInput("sslpassword");
+    const sslSecretPassword = core.getInput("sslsecretpassword");
+    const sslClientId =
+      core.getInput("sslclientid") ||
+      "qOUeZCCzSqgA93acB3LYq6lBNjgZdiOxQc-KayC3UMw";
+    const isTestStr = core.getInput("istest") || "true";
+    const isTest = isTestStr !== "false";
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    // Sandbox credentials are publicly available at https://www.ssl.com/guide/esigner-demo-credentials-and-certificates/
 
-    core.setOutput('time', new Date().toTimeString());
+    core.info(
+      `Running windows sign action as test: [${isTest}] for [${filePath}] ...`
+    );
+    const zipFile = "CodeSignTool.zip";
+
+    core.info("---Downloading zip");
+    request
+      .get("https://www.ssl.com/download/codesigntool-for-windows/")
+      .on("error", function (error) {
+        core.error(error);
+        core.setFailed(error.message);
+        return;
+      })
+      .pipe(fs.createWriteStream(__dirname + "/" + zipFile))
+      .on("finish", function () {
+        core.info("---Finished downloading zip");
+        var zip = new admZip(__dirname + "/" + zipFile);
+        core.info("---Start unzip");
+        zip.extractAllTo(__dirname + "/", true);
+        core.info("---Finished unzip");
+        let foundUnzipped = fs
+          .readdirSync(__dirname + "/")
+          .filter((fn) => fn.startsWith("CodeSignTool-v"));
+        if (!foundUnzipped || foundUnzipped.length == 0) {
+          core.error("Could not find unzipped CodeSignTool");
+          core.setFailed(error.message);
+          return;
+        }
+        const folder = foundUnzipped[0];
+        core.info(`---Using unzipped folder: [${folder}]`);
+
+        exec("pwd", function (err, stdout, stderr) {
+          core.info("--PWD:  " + stdout);
+          const pwd = stdout.trim();
+
+          core.info(
+            "CODE_SIGN_TOOL_PATH-before: \t" + process.env.CODE_SIGN_TOOL_PATH
+          );
+          process.env.CODE_SIGN_TOOL_PATH = `${__dirname}\\${folder}`;
+          core.info(
+            "CODE_SIGN_TOOL_PATH-after: \t" + process.env.CODE_SIGN_TOOL_PATH
+          );
+
+          core.info("__dirname: \t" + __dirname);
+
+          core.info(`\t${isTest ? "RUNNING TEST" : "RUNNING REAL USE CASE"}`);
+
+          let content = isTest
+            ? `CLIENT_ID=${sslClientId}\nOAUTH2_ENDPOINT=https://oauth-sandbox.ssl.com/oauth2/token\nCSC_API_ENDPOINT=https://cs-try.ssl.com\nTSA_URL=http://ts.ssl.com`
+            : `CLIENT_ID=${sslClientId}\nOAUTH2_ENDPOINT=https://login.ssl.com/oauth2/token\nCSC_API_ENDPOINT=https://cs.ssl.com\nTSA_URL=http://ts.ssl.com`;
+
+          core.info(`---Writing updated conf file`);
+          try {
+            fs.writeFileSync(
+              `${process.env.CODE_SIGN_TOOL_PATH}/conf/code_sign_tool.properties`,
+              content,
+              { encoding: "utf8", flag: "w" }
+            );
+            // file written successfully
+          } catch (err) {
+            core.error(err);
+            core.setFailed(err);
+            return;
+          }
+          core.info(`---Executing SIGN Action`);
+          exec(
+            isTest
+              ? `${process.env.CODE_SIGN_TOOL_PATH}/CodeSignTool.bat sign -username='esigner_demo' -password='esignerDemo#1' -totp_secret='RDXYgV9qju+6/7GnMf1vCbKexXVJmUVr+86Wq/8aIGg=' -input_file_path="${filePath}" -override`
+              : `${process.env.CODE_SIGN_TOOL_PATH}/CodeSignTool.bat sign -username="${sslUsername}" -password="${sslPassword}" -totp_secret="${sslSecretPassword}" -input_file_path="${filePath}" -override`,
+            function (err, stdout, stderr) {
+              if (err || stderr) {
+                core.error(stderr);
+                core.error(err);
+                core.setFailed(stderr);
+                return;
+              }
+              // Done.
+              core.info("---Done SIGNING, check for error");
+              if (stdout.includes("Error")) {
+                core.error(stdout);
+                core.setFailed(stdout);
+                return;
+              } else {
+                core.info(stdout);
+                core.info("---SUCCESS");
+              }
+            }
+          );
+        });
+      });
   } catch (error) {
+    core.info(error);
     core.setFailed(error.message);
+    return;
   }
 }
 
